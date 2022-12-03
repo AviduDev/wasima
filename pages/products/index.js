5234f0d9e37e943686575f5caa8e5407cc1eb1e8
AviduDev <avidudev@gmail.com>
2022-12-03 14:34:29 +0530

    Product page basic styling done

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,9 +1,63 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from "../../styles/Products.module.css";
+
 export default function Products() {
-    return (
-        <div>
-            <main>
-                <h1>Products Page</h1>
-            </main>
+  return (
+    <div>
+      <main className={styles.main}>
+        <div className={styles.pagetitle}>
+          <h1>products</h1>
         </div>
-    )
+        <div className={styles.tags_holder}>
+          <div className={styles.category_tag}>Short Sleeve</div>
+          <div className={styles.category_tag}>Long Sleeve</div>
+          <div className={styles.category_tag}>Sweaters</div>
+          <div className={styles.category_tag}>Hoodies</div>
+          <div className={styles.category_tag}>Jackets</div>
+        </div>
+
+        <div className={styles.product_grid}>
+          <div className={styles.product_item}>
+            <div className={styles.product_image_container}>
+              <img className={styles.product_image} src="product1.jpg" alt="" />
+              <p className={styles.tag}>NEW</p>
+              <button className={styles.addtocart}>ADD TO CART</button>
+            </div>
+            <h3 className={styles.product_name}>Product Name</h3>
+            <p className={styles.product_price}>1620 LKR</p>
+          </div>
+
+          <div className={styles.product_item}>
+            <div className={styles.product_image_container}>
+              <img className={styles.product_image} src="product2.jpg" alt="" />
+              <p className={styles.tag}>NEW</p>
+              <button className={styles.addtocart}>ADD TO CART</button>
+            </div>
+            <h3 className={styles.product_name}>Product Name</h3>
+            <p className={styles.product_price}>1620 LKR</p>
+          </div>
+
+          <div className={styles.product_item}>
+            <div className={styles.product_image_container}>
+              <img className={styles.product_image} src="product3.jpg" alt="" />
+              <p className={styles.tag}>NEW</p>
+              <button className={styles.addtocart}>ADD TO CART</button>
+            </div>
+            <h3 className={styles.product_name}>Product Name</h3>
+            <p className={styles.product_price}>1620 LKR</p>
+          </div>
+
+          <div className={styles.product_item}>
+            <div className={styles.product_image_container}>
+              <img className={styles.product_image} src="product4.jpg" alt="" />
+              <p className={styles.tag}>NEW</p>
+              <button className={styles.addtocart}>ADD TO CART</button>
+            </div>
+            <h3 className={styles.product_name}>Product Name</h3>
+            <p className={styles.product_price}>1620 LKR</p>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
