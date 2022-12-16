50755d24210f6789e4563b9b24c7dd2e89ee8618
AviduDev <avidudev@gmail.com>
2022-12-16 07:22:37 +0530

    form added

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,8 +1,21 @@
+import styles from '../../styles/Contact.module.css'
+
 export default function Contact() {
     return (
         <div>
-            <main>
-                <h1>Contact Page</h1>
+            <main className={styles.main}>
+                <div className={styles.pagetitle}>
+                <h1>contact us</h1>
+                </div>
+
+                
+                    <form action="https://formsubmit.co/avidu97@gmail.com" method="post">
+                        <input type="text" name='name' placeholder='Your Name' />
+                        <input type="email" name="email" id="" placeholder='Your Email' />
+                        <button type="submit">SEND</button>
+                    </form>
+           
+                
             </main>
         </div>
     )
