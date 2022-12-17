5eb74f1e6a7e7e37e0ca930ffa48506ed807b692
AviduDev <avidudev@gmail.com>
2022-12-17 07:01:39 +0530

    map added

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,22 +1,41 @@
-import styles from '../../styles/Contact.module.css'
+import styles from "../../styles/Contact.module.css";
+
+import Map from "../../components";
+
+const DEFAULT_CENTER = [38.907132, -77.036546];
 
 export default function Contact() {
-    return (
-        <div>
-            <main className={styles.main}>
-                <div className={styles.pagetitle}>
-                <h1>contact us</h1>
-                </div>
-
-                
-                    <form action="https://formsubmit.co/avidu97@gmail.com" method="post">
-                        <input type="text" name='name' placeholder='Your Name' />
-                        <input type="email" name="email" id="" placeholder='Your Email' />
-                        <button type="submit">SEND</button>
-                    </form>
-           
-                
-            </main>
+  return (
+    <div>
+      <main className={styles.main}>
+        <div className={styles.pagetitle}>
+          <h1>contact us</h1>
         </div>
-    )
+
+        <form action="https://formsubmit.co/avidu97@gmail.com" method="post">
+          <input type="text" name="name" placeholder="Your Name" />
+          <input type="email" name="email" id="" placeholder="Your Email" />
+          <button type="submit">SEND</button>
+        </form>
+
+        <div>
+          <Map className={styles.homeMap} center={DEFAULT_CENTER} zoom={12}>
+            {({ TileLayer, Marker, Popup }) => (
+              <>
+                <TileLayer
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={DEFAULT_CENTER}>
+                  <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                  </Popup>
+                </Marker>
+              </>
+            )}
+          </Map>
+        </div>
+      </main>
+    </div>
+  );
 }
