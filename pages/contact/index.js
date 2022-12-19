2b7cc6ded91878e33f0fa3556956f1cfcb50b880
AviduDev <avidudev@gmail.com>
2022-12-19 21:44:43 +0530

    contact form added

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styles from "../../styles/Contact.module.css";
 
 import dynamic from "next/dynamic";
@@ -13,13 +14,62 @@ export default function Contact() {
         <div className={styles.pagetitle}>
           <h1>contact us</h1>
         </div>
+        <div className={styles.contactContainer}>
+          <form
+            className={styles.form}
+            action="https://formsubmit.co/avidu97@gmail.com"
+            method="post"
+          >
+            <label htmlFor="name">
+              Name
+              <input type="text" name="name" placeholder="Your Name" required />
+            </label>
 
-        <form action="https://formsubmit.co/avidu97@gmail.com" method="post">
-          <input type="text" name="name" placeholder="Your Name" />
-          <input type="email" name="email" id="" placeholder="Your Email" />
-          <button type="submit">SEND</button>
-        </form>
+            <br />
+            <input type="hidden" name="_subject" value="New submission!" />
+            <label htmlFor="email">
+              Email
+              <input
+                type="email"
+                name="email"
+                id=""
+                placeholder="Your Email"
+                required
+              />
+            </label>
 
+            <br />
+            <input
+              type="hidden"
+              name="_next"
+              value="https://yourdomain.co/thanks.html"
+            />
+            {/* <br />
+          <input type="file" name="attachment" accept="image/png, image/jpeg" /> */}
+            <br />
+            <textarea name="message" placeholder="Write Something" />
+            <br />
+            <input type="hidden" name="_captcha" value="false" />
+            <button type="submit">SEND</button>
+          </form>
+          <div className={styles.contactDetails}>
+            <address>Colombo 10</address>
+            <a href="tel:+94767578127">+9476 75 78 127</a>
+            <br />
+            <a href="mailto:avidu97@gmail.com">avidu97@gmail.com</a>
+            <div className={styles.socialMedia}>
+              <Link className={styles.socialIcon} href="https://www.facebook.com/">
+                <img src="facebook.svg" alt="" />
+              </Link>
+              <Link className={styles.socialIcon} href="https://www.facebook.com/">
+                <img src="instagram.svg" alt="" />
+              </Link>
+              <Link className={styles.socialIcon} href="https://www.facebook.com/">
+                <img src="linkedin.svg" alt="" />
+              </Link>
+            </div>
+          </div>
+        </div>
         <div>
           <MapWithNoSSR />
         </div>
